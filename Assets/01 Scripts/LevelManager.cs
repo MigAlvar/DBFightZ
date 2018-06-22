794a4bd91089936ad46ea1dcc6e639bd13697973
MigAlvar <migjig.20@gmail.com>
2018-06-22 12:56:28 -0400

    Core Setup

---
+++ Assets/01 Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	private Musical MscManager;
+
+	void ChangeLevel(int index){
+		SceneManager.LoadScene(index);
+		MscManager.LevelMusic(index);
+	}
+	
+}
---
+++ Assets/01 Scripts/Musical.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musical : MonoBehaviour {
+
+	[Header("Level Theme Music")]
+	[SerializeField] private List <AudioClip> music = new List <AudioClip>();
+	private AudioSource Snd;
+	private float vol, MAX = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		Snd = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void LevelMusic(int index){
+			if(music[index] == null){
+				Snd.Stop();
+			}
+			else{
+				Snd.clip = music[index];
+				Snd.Play();
+			}
+	}
+
+	void Fader(bool io){
+		if(io){
+			while(vol < MAX){
+				vol += Time.deltaTime/10;
+			}
+		}else{
+			while(vol > 0){
+				vol -= Time.deltaTime/10;
+			}
+		}
+
+		Snd.volume = vol;
+	}
+}
